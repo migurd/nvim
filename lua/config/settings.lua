f5571930cd83c6b22ec68537bbcaeca1cb156e98
angelq <angelquimorita@gmail.com>
2023-12-30 22:40:44 -0600

    Fugitive shortcuts added

--- lua/config/settings.lua
+++ lua/config/settings.lua
@@ -1,10 +1,8 @@
 vim.o.number = true
+vim.g.mapleader = " "
 --vim.cmd.colorscheme("slate")
 
 -- Set tab width to 4 spaces
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 vim.o.expandtab = true
-
--- Colors
-vim.o.colorscheme = "catppuccin-mocha"
--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,3 +1,10 @@
 return {
     "tpope/vim-fugitive",
+    cmd = {"G", "Git"}, -- Loads once either command is used
+    keys = {
+        {"<leader>ga", ":Git fetch --all -p<cr>", desc = "Git fetch"},
+        {"<leader>gl", ":Git pull<cr>", desc = "Git pull"},
+        {"<leader>gp", ":Git push<cr>", desc = "Git push"},
+        {"<leader>gs", ":Git status<cr>", desc = "Git status"},
+    },
 }
