9229649fa37e9e8c0b57da2728faa3d9d6dedc14
angelq <angelquimorita@gmail.com>
2024-01-03 14:13:41 -0600

    Telescope was added successfully

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,5 +1,11 @@
 return {
     'nvim-telescope/telescope.nvim', tag = '0.1.5',
     -- or                              , branch = '0.1.x',
-    dependencies = { 'nvim-lua/plenary.nvim' }
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        'nvim-tree/nvim-web-devicons',
+   },
+    config = function()
+        require "angelq.telescope"
+    end,
 }
