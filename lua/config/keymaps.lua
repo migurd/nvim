5dcafd42761fdfaceaec80ebb613b136953bd2ee
angelq <angelquimorita@gmail.com>
2024-01-14 01:46:56 -0600

    Added some keymaps from ThePrimeagen

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,1 +1,19 @@
 vim.keymap.set("n", "<leader>h", vim.cmd.Ex)
+
+-- Love you Primeagen
+vim.keymap.set("v", "<M-j>", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "<M-k>", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "NzzzV")
+
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+vim.keymap.set("n", "<leader>d", "\"_d")
+vim.keymap.set("v", "<leader>d", "\"_d")
+
+-- skill issue
+vim.keymap.set("i", "<C-c>", "<Esc>")
