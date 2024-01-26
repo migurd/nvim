d7ee356102023f07108fd0400385976034432312
angelq <angelquimorita@gmail.com>
2024-01-25 22:26:11 -0600

    Added epic web settings

--- lua/config/settings.lua
+++ lua/config/settings.lua
@@ -2,7 +2,8 @@ vim.o.number = true
 vim.g.mapleader = " "
 vim.o.relativenumber = true
 vim.o.cursorline = true
---vim.cmd.colorscheme("slate")
+-- vim.cmd.colorscheme("slate")
+vim.o.colorscheme = "tokyonight-night"
 
 -- Set tab width to 4 spaces
 vim.o.tabstop = 4
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -55,6 +55,11 @@ return {
         })
         lspconfig.lua_ls.setup({
             on_attach = on_attach,
+            settings = {
+                Lua = {
+                    diagnostics = {globals = {'vim'}},
+                },
+            },
         })
         lspconfig.tsserver.setup{
             on_attach = on_attach,
